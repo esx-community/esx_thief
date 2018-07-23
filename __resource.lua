21d582b833b31fb9725a8c7345d1db6847d32e2b
MajorMarcin <zelkowski.m@gmail.com>
2018-07-23 15:14:42 +0200

    add pl

--- __resource.lua
+++ __resource.lua
@@ -9,6 +9,7 @@ client_scripts {
   'locales/en.lua',
   'locales/fr.lua',
   'locales/sv.lua',
+  'locales/pl.lua',
   'config.lua',
   'client/main.lua',
   'handsup.lua'
@@ -19,6 +20,7 @@ server_scripts {
   'locales/en.lua',
   'locales/fr.lua',
   'locales/sv.lua',
+  'locales/pl.lua',
   'config.lua',
   'server/main.lua'
 }
