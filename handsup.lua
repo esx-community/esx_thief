d9d9aa8e3da3a980d228e3f1c13c808e1a803b63
ElPumpo <samo_kol@hotmail.com>
2019-03-15 13:27:28 +0100

    Proper indenting

--- handsup.lua
+++ handsup.lua
@@ -1,64 +1,62 @@
 local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-  }
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
 
 local canHandsUp = true
-local GUI							= {}
-GUI.Time							= 0
 
-AddEventHandler("handsup:toggle", function(param)
+AddEventHandler('handsup:toggle', function(param)
 	canHandsUp = param
 end)
 
 Citizen.CreateThread(function()
 	local handsup = false
+
 	while true do
-		Wait(0)
-		local lPed = GetPlayerPed(-1)
-		RequestAnimDict("random@mugging3")
+		Citizen.Wait(0)
+		local playerPed = GetPlayerPed(-1)
+		RequestAnimDict('random@mugging3')
+
 		if canHandsUp then
-			if (IsControlPressed(1, Keys['X']) and (GetGameTimer() - GUI.Time) > 150) then
+			if IsControlJustReleased(0, Keys['X']) then
 				if handsup then
-					if DoesEntityExist(lPed) then
+					if DoesEntityExist(playerPed) then
 						Citizen.CreateThread(function()
-							RequestAnimDict("random@mugging3")
-							while not HasAnimDictLoaded("random@mugging3") do
+							RequestAnimDict('random@mugging3')
+							while not HasAnimDictLoaded('random@mugging3') do
 								Citizen.Wait(100)
 							end
 
 							if handsup then
 								handsup = false
-								ClearPedSecondaryTask(lPed)
-                TriggerServerEvent("esx_thief:update", handsup)
+								ClearPedSecondaryTask(playerPed)
+								TriggerServerEvent('esx_thief:update', handsup)
 							end
 						end)
 					end
 				else
-					if DoesEntityExist(lPed) then
+					if DoesEntityExist(playerPed) then
 						Citizen.CreateThread(function()
-							RequestAnimDict("random@mugging3")
-							while not HasAnimDictLoaded("random@mugging3") do
+							RequestAnimDict('random@mugging3')
+							while not HasAnimDictLoaded('random@mugging3') do
 								Citizen.Wait(100)
 							end
 
 							if not handsup then
 								handsup = true
-								TaskPlayAnim(lPed, "random@mugging3", "handsup_standing_base", 6.0, -6.0, -1, 49, 0, 0, 0, 0)
-                TriggerServerEvent("esx_thief:update", handsup)
+								TaskPlayAnim(playerPed, 'random@mugging3', 'handsup_standing_base', 6.0, -6.0, -1, 49, 0, 0, 0, 0)
+								TriggerServerEvent('esx_thief:update', handsup)
 							end
 						end)
 					end
 				end
-				
-				GUI.Time  = GetGameTimer()
 			end
 
 		end
--- server/main.lua
+++ server/main.lua
@@ -1,116 +1,96 @@
-ESX 				= nil
-local Users         = {}
+ESX = nil
+local Users = {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback('esx_thief:getValue', function(source, cb, targetSID)
-    if Users[targetSID] then
-        cb(Users[targetSID])
-    else
-        cb({value = false, time = 0})
-    end
+	if Users[targetSID] then
+		cb(Users[targetSID])
+	else
+		cb({value = false, time = 0})
+	end
 end)
 
 ESX.RegisterServerCallback('esx_thief:getOtherPlayerData', function(source, cb, target)
+	local xPlayer = ESX.GetPlayerFromId(target)
 
-  local _target = target
-  local xPlayer = ESX.GetPlayerFromId(_target)
+	local data = {
+		name = GetPlayerName(target),
+		inventory = xPlayer.inventory,
+		accounts = xPlayer.accounts,
+		money = xPlayer.getMoney()
+	}
 
-    local data = {
-      name        = GetPlayerName(target),
-      inventory   = xPlayer.inventory,
-      accounts    = xPlayer.accounts,
-      money       = xPlayer.get('money')
-
-    }
-
-      cb(data)
-
+	cb(data)
 end)
-
 
 RegisterServerEvent('esx_thief:stealPlayerItem')
 AddEventHandler('esx_thief:stealPlayerItem', function(target, itemType, itemName, amount)
+	local _source = source
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
 
-    local _source = source
-    local _target = target
-    local sourceXPlayer = ESX.GetPlayerFromId(_source)
-    local targetXPlayer = ESX.GetPlayerFromId(_target)
+	if itemType == 'item_standard' then
 
-    if itemType == 'item_standard' then
+		local label = sourceXPlayer.getInventoryItem(itemName).label
+		local itemLimit = sourceXPlayer.getInventoryItem(itemName).limit
+		local sourceItemCount = sourceXPlayer.getInventoryItem(itemName).count
+		local targetItemCount = targetXPlayer.getInventoryItem(itemName).count
 
-        local label = sourceXPlayer.getInventoryItem(itemName).label
-        local itemLimit = sourceXPlayer.getInventoryItem(itemName).limit
-        local sourceItemCount = sourceXPlayer.getInventoryItem(itemName).count
-        local targetItemCount = targetXPlayer.getInventoryItem(itemName).count
+		if amount > 0 and targetItemCount >= amount then
+			if itemLimit ~= -1 and (sourceItemCount + amount) > itemLimit then
+				TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('ex_inv_lim_target'))
+				TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('ex_inv_lim_source'))
+			else
+				targetXPlayer.removeInventoryItem(itemName, amount)
+				sourceXPlayer.addInventoryItem(itemName, amount)
 
-        if amount > 0 and targetItemCount >= amount then
-    
-            if itemLimit ~= -1 and (sourceItemCount + amount) > itemLimit then
-                TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('ex_inv_lim_target'))
-                TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('ex_inv_lim_source'))
-            else
+				TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~x' .. amount .. ' ' .. label .. ' ~w~' .. _U('from_your_target') )
+				TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~x'  .. amount .. ' ' .. label )
+			end
+		else
+			TriggerClientEvent('esx:showNotification', _source, _U('invalid_quantity'))
+		end
 
-                targetXPlayer.removeInventoryItem(itemName, amount)
-                sourceXPlayer.addInventoryItem(itemName, amount)
+	elseif itemType == 'item_money' then
 
-                TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~x' .. amount .. ' ' .. label .. ' ~w~' .. _U('from_your_target') )
-                TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~x'  .. amount .. ' ' .. label )
+		if amount > 0 and targetXPlayer.get('money') >= amount then
+			targetXPlayer.removeMoney(amount)
+			sourceXPlayer.addMoney(amount)
 
-            end
+			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~$' .. amount .. ' ~w~' .. _U('from_your_target') )
+			TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~$'  .. amount )
+		else
+			TriggerClientEvent('esx:showNotification', _source, _U('imp_invalid_amount'))
+		end
 
-        else
-             TriggerClientEvent('esx:showNotification', _source, _U('invalid_quantity'))
-        end
+	elseif itemType == 'item_account' then
 
-    end
+		if amount > 0 and targetXPlayer.getAccount(itemName).money >= amount then
+			targetXPlayer.removeAccountMoney(itemName, amount)
+			sourceXPlayer.addAccountMoney(itemName, amount)
 
-  if itemType == 'item_money' then
+			TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~$' .. amount .. ' ~w~' .. _U('of_black_money') .. ' ' .. _U('from_your_target') )
+			TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~$'  .. amount .. ' ~w~' .. _U('of_black_money') )
+		else
+			TriggerClientEvent('esx:showNotification', _source, _U('imp_invalid_amount'))
+		end
 
-    if amount > 0 and targetXPlayer.get('money') >= amount then
-
-      targetXPlayer.removeMoney(amount)
-      sourceXPlayer.addMoney(amount)
-
-      TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~$' .. amount .. ' ~w~' .. _U('from_your_target') )
-      TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~$'  .. amount )
-
-    else
-      TriggerClientEvent('esx:showNotification', _source, _U('imp_invalid_amount'))
-    end
-
-  end
-
-  if itemType == 'item_account' then
-
-    if amount > 0 and targetXPlayer.getAccount(itemName).money >= amount then
-
-        targetXPlayer.removeAccountMoney(itemName, amount)
-        sourceXPlayer.addAccountMoney(itemName, amount)
-
-        TriggerClientEvent('esx:showNotification', sourceXPlayer.source, _U('you_stole') .. ' ~g~$' .. amount .. ' ~w~' .. _U('of_black_money') .. ' ' .. _U('from_your_target') )
-        TriggerClientEvent('esx:showNotification', targetXPlayer.source, _U('someone_stole') .. ' ~r~$'  .. amount .. ' ~w~' .. _U('of_black_money') )
-
-    else
-        TriggerClientEvent('esx:showNotification', _source, _U('imp_invalid_amount'))
-    end
-
-  end
-
+	end
 end)
 
-RegisterServerEvent("esx_thief:update")
-AddEventHandler("esx_thief:update", function(bool)
+RegisterServerEvent('esx_thief:update')
+AddEventHandler('esx_thief:update', function(bool)
 	local _source = source
 	Users[_source] = {value = bool, time = os.time()}
 end)
 
-RegisterServerEvent("esx_thief:getValue")
-AddEventHandler("esx_thief:getValue", function(targetSID)
-    local _source = source
+RegisterServerEvent('esx_thief:getValue')
+AddEventHandler('esx_thief:getValue', function(targetSID)
+	local _source = source
 	if Users[targetSID] then
-		TriggerClientEvent("esx_thief:returnValue", _source, Users[targetSID])
+		TriggerClientEvent('esx_thief:returnValue', _source, Users[targetSID])
 	else
-		TriggerClientEvent("esx_thief:returnValue", _source, Users[targetSID])
+		TriggerClientEvent('esx_thief:returnValue', _source, Users[targetSID])
 	end
 end)
