6753a442b2fdb52b5a3a933b116e2068faa401f8
ElPumpo <samo_kol@hotmail.com>
2019-03-21 22:28:00 +0100

    Simplified handsup

--- handsup.lua
+++ handsup.lua
@@ -21,44 +21,26 @@ Citizen.CreateThread(function()
 
 	while true do
 		Citizen.Wait(0)
-		local playerPed = GetPlayerPed(-1)
-		RequestAnimDict('random@mugging3')
 
 		if canHandsUp then
 			if IsControlJustReleased(0, Keys['X']) then
+				local playerPed = PlayerPedId()
+
+				RequestAnimDict('random@mugging3')
+				while not HasAnimDictLoaded('random@mugging3') do
+					Citizen.Wait(100)
+				end
+
 				if handsup then
-					if DoesEntityExist(playerPed) then
-						Citizen.CreateThread(function()
-							RequestAnimDict('random@mugging3')
-							while not HasAnimDictLoaded('random@mugging3') do
-								Citizen.Wait(100)
-							end
-
-							if handsup then
-								handsup = false
-								ClearPedSecondaryTask(playerPed)
-								TriggerServerEvent('esx_thief:update', handsup)
-							end
-						end)
-					end
+					handsup = false
+					ClearPedSecondaryTask(playerPed)
+					TriggerServerEvent('esx_thief:update', handsup)
 				else
-					if DoesEntityExist(playerPed) then
-						Citizen.CreateThread(function()
-							RequestAnimDict('random@mugging3')
-							while not HasAnimDictLoaded('random@mugging3') do
-								Citizen.Wait(100)
-							end
-
-							if not handsup then
-								handsup = true
-								TaskPlayAnim(playerPed, 'random@mugging3', 'handsup_standing_base', 6.0, -6.0, -1, 49, 0, 0, 0, 0)
-								TriggerServerEvent('esx_thief:update', handsup)
-							end
-						end)
-					end
+					handsup = true
+					TaskPlayAnim(playerPed, 'random@mugging3', 'handsup_standing_base', 6.0, -6.0, -1, 49, 0, 0, 0, 0)
+					TriggerServerEvent('esx_thief:update', handsup)
 				end
 			end
-
 		end
 	end
 end)
